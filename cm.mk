c0e6771c43d4fa4568727265715aed754d693412
Nikhil P E <nikhilpe@gmail.com>
2016-04-28 20:51:36 +0530

    initial commit

---
+++ cm.mk
@@ -0,0 +1,18 @@
+# Inherit AOSP Rhine device parts
+$(call inherit-product, device/sony/honami/aosp_c6903.mk)
+
+# Inherit aicp Rhine device parts
+$(call inherit-product, device/sony/rhine/platform2.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.0.2/14.5.A.0.270/3750474323:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.0.2 14.5.A.0.270 3750474323 release-keys"
+
+PRODUCT_NAME := cm_honami
+PRODUCT_DEVICE := honami
